c7b6cda6af5ca5246cb6d0e28ea82db2e6504fd9
Brendan Dileo <dileob23@gmail.com>
2025-06-08 12:22:10 -0400

    Implement size and is empty methods

--- src/Heap.java
+++ src/Heap.java
@@ -1,2 +1,28 @@
-public class Heap {
+import java.util.ArrayList;
+
+
+// Generic implementation of a Heap data structure
+public class Heap<T extends Comparable<T>> {
+
+
+    private final ArrayList<T> list;
+
+    // Constructs a heap
+    public Heap() {
+        list = new ArrayList<>();
+    }
+
+    // Returns the size of the heap
+    public int size() {
+        return list.size();
+    }
+
+    // Checks if heap is emty
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+
+
+
 }
